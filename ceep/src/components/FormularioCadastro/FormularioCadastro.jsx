acecd93ffd71ff446c35b4304cc065fb02751e68
Luiz Henrique <luizhenrique.se@gmail.com>
2020-08-31 21:38:29 +0000

    feat: creating handle for onchange title

--- ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,25 +1,37 @@
 import React, { Component } from "react";
 import "./estilo.css";
 class FormularioCadastro extends Component {
-  render() {
-    return (
-      <form className="form-cadastro ">
-        <input
-          type="text"
-          placeholder="Título"
-          className="form-cadastro_input"
-        />
-        <textarea
-          rows={15}
-          placeholder="Escreva sua nota..."
-          className="form-cadastro_input"
-        />
-        <button className="form-cadastro_input form-cadastro_submit">
-          Criar Nota
+
+    constructor(){
+        super();
+        this.titulo="";
+    }
+
+    handleMudancaTitulo(event) {
+        this.titulo = event.target.value;
+        console.log(this.titulo);
+    }
+
+    render() {
+        return (
+            <form className="form-cadastro ">
+                <input
+                    type="text"
+                    placeholder="Título"
+                    className="form-cadastro_input"
+                    onChange={this.handleMudancaTitulo.bind(this)}
+                />
+                <textarea
+                    rows={15}
+                    placeholder="Escreva sua nota..."
+                    className="form-cadastro_input"
+                />
+                <button className="form-cadastro_input form-cadastro_submit">
+                    Criar Nota
         </button>
-      </form>
-    );
-  }
+            </form>
+        );
+    }
 }
 
 export default FormularioCadastro;
