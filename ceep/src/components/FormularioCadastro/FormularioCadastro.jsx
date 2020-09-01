238f3b0b3142bc17aeed392cf58ec1bb9351175c
Luiz Henrique <luizhenrique.se@gmail.com>
2020-09-01 19:48:03 +0000

    feat: working with proprieties between components

--- ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,29 +2,45 @@ import React, { Component } from "react";
 import "./estilo.css";
 class FormularioCadastro extends Component {
 
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.titulo="";
+        this.texto="";
     }
 
-    handleMudancaTitulo(event) {
+    _handleMudancaTitulo(event) {
+        event.stopPropagation();
         this.titulo = event.target.value;
         console.log(this.titulo);
     }
 
+    _handleMudancaTexto(event) {
+        event.stopPropagation();
+        this.texto = event.target.value;
+        console.log(this.texto);
+    }
+
+    _criarNota(event){
+        event.preventDefault();
+        event.stopPropagation();
+        this.props.criarNota(this.titulo, this.texto);
+    }
+
     render() {
         return (
-            <form className="form-cadastro ">
+            <form className="form-cadastro" 
+            onSubmit={this._criarNota.bind(this)}>
                 <input
                     type="text"
                     placeholder="Título"
                     className="form-cadastro_input"
-                    onChange={this.handleMudancaTitulo.bind(this)}
+                    onChange={this._handleMudancaTitulo.bind(this)}
                 />
                 <textarea
                     rows={15}
                     placeholder="Escreva sua nota..."
                     className="form-cadastro_input"
+                    onChange={this._handleMudancaTexto.bind(this)}
                 />
                 <button className="form-cadastro_input form-cadastro_submit">
                     Criar Nota
